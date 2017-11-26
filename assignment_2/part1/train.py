0a91ee0f0438b5a98ded5b84888da36b7852c708
Jose Gallego <jgalle29@gmail.com>
2017-11-26 01:44:19 +0100

    Vanilla RNN

---
+++ assignment_2/part1/train.py
@@ -0,0 +1,166 @@
+# MIT License
+#
+# Copyright (c) 2017 Tom Runia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Tom Runia
+# Date Created: 2017-10-19
+################################################################################
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import time
+from datetime import datetime
+
+import numpy as np
+import tensorflow as tf
+
+import utils
+from vanilla_rnn import VanillaRNN
+from lstm import LSTM
+
+################################################################################
+
+def dense_to_one_hot(labels_dense, num_classes):
+    """
+    Convert class labels from scalars to one-hot vectors.
+    Args:
+    labels_dense: Dense labels.
+    num_classes: Number of classes.
+
+    Outputs:
+    labels_one_hot: One-hot encoding for labels.
+    """
+
+    num_labels = labels_dense.shape[0]
+    index_offset = np.arange(num_labels) * num_classes
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+def train(config):
+
+    assert config.model_type in ('RNN', 'LSTM')
+
+    # Setup the model that we are going to use
+    if config.model_type == 'RNN':
+        print("Initializing Vanilla RNN model...")
+        model = VanillaRNN(
+            config.input_length - 1, config.input_dim, config.num_hidden,
+            config.num_classes, config.batch_size
+        )
+    else:
+        print("Initializing LSTM model...")
+        model = LSTM(
+            config.input_length - 1, config.input_dim, config.num_hidden,
+            config.num_classes, config.batch_size
+        )
+
+    ###########################################################################
+    # Implement code here.
+    ###########################################################################
+
+    sess = tf.Session()
+
+    # Setup global step
+    global_step = tf.Variable(0, trainable=False, name='global_step')
+
+    # Define the optimizer
+    assert config.optimizer in ('adam', 'rmsprop')
+    if config.optimizer == "adam":
+        optimizer = tf.train.AdamOptimizer(config.learning_rate)
+    elif config.optimizer == "rmsprop":
+        optimizer = tf.train.RMSPropOptimizer(config.learning_rate)
+
+    # Define summary operation
+    summary_op = tf.summary.merge_all()
+
+    ###########################################################################
+    # QUESTION: what happens here and why?
+    # ANSWER: we calculate the gradients and clip each of them if the magnitude
+    #         is larger than config.max_norm_gradient to avoid unstable learning
+    #         due to exploding gradients in cliffs in the loss surface.
+    ###########################################################################
+    grads_and_vars = optimizer.compute_gradients(model._loss)
+    grads, variables = zip(*grads_and_vars)
+    grads_clipped, _ = tf.clip_by_global_norm(grads, clip_norm = config.max_norm_gradient)
+    apply_gradients_op = optimizer.apply_gradients(zip(grads_clipped, variables), global_step = global_step)
+    ############################################################################
+
+    init_op = tf.global_variables_initializer()
+    local_init_op = tf.local_variables_initializer()
+    sess.run(fetches=[init_op, local_init_op])
+
+    ###########################################################################
+    # Implement code here.
+    ###########################################################################
+
+    palindrome_length = config.input_length
+
+    for train_step in range(config.train_steps):
+
+        # Only for time measurement of step through network
+        t1 = time.time()
+
+        palindrome_batch = utils.generate_palindrome_batch(config.batch_size, palindrome_length)
+        x_dense = palindrome_batch[:,:-1]
+        y_dense = palindrome_batch[:,-1]
+
+        x = np.transpose((np.arange(model._num_classes) == x_dense[...,None]).astype(int), [1, 0, 2])
+        y = (np.arange(model._num_classes) == y_dense[..., None]).astype(int)
+
+        tr_feed = {model._inputs: x, model._targets: y}
+        fetches = [apply_gradients_op, model._loss, model._accuracy]
+
+        _, train_loss, train_accuracy = sess.run(fetches=fetches, feed_dict=tr_feed)
+
+        # Only for time measurement of step through network
+        t2 = time.time()
+        examples_per_second = config.batch_size/float(t2-t1)
+
+        # Print the training progress
+        if train_step % config.print_every == 0:
+            print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, "
+                  "Examples/Sec = {:.2f}, Accuracy = {:.3f}, Loss = {:.4f}".format(
+                  datetime.now().strftime("%Y-%m-%d %H:%M"), train_step,
+                  config.train_steps, config.batch_size, examples_per_second,
+                  train_accuracy, train_loss))
+
+
+if __name__ == "__main__":
+
+    # Parse training configuration
+    parser = argparse.ArgumentParser()
+
+    # Model params
+    parser.add_argument('--model_type', type=str, default="RNN", help="Model type, should be 'RNN' or 'LSTM'")
+    parser.add_argument('--input_length', type=int, default=5, help='Length of an input sequence')
+    parser.add_argument('--input_dim', type=int, default=10, help='Dimensionality of input sequence')
+    parser.add_argument('--num_classes', type=int, default=10, help='Dimensionality of output sequence')
+    parser.add_argument('--num_hidden', type=int, default=128, help='Number of hidden units in the model')
+
+    # Training params
+    parser.add_argument('--batch_size', type=int, default=128, help='Number of examples to process in a batch')
+    parser.add_argument('--learning_rate', type=float, default=0.025, help='Learning rate')
+    parser.add_argument('--train_steps', type=int, default=2500, help='Number of training steps')
+    parser.add_argument('--max_norm_gradient', type=float, default=10.0, help='--')
+
+    # Misc params
+    parser.add_argument('--summary_path', type=str, default="./summaries/", help='Output path for summaries')
+    parser.add_argument('--print_every', type=int, default=5, help='How often to print training progress')
+
+    parser.add_argument('--optimizer', type=str, default="adam", help='Optimizer to use')
+
+    config = parser.parse_args()
+
+    # Train the model
+    train(config)
---
+++ assignment_2/part1/vanilla_rnn.py
@@ -0,0 +1,123 @@
+# MIT License
+#
+# Copyright (c) 2017 Tom Runia
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Tom Runia
+# Date Created: 2017-10-19
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+#import numpy as np
+import tensorflow as tf
+
+################################################################################
+
+class VanillaRNN(object):
+
+    def __init__(self, input_length, input_dim, num_hidden, num_classes, batch_size):
+
+        self._input_length = input_length
+        self._input_dim = input_dim
+        self._num_hidden = num_hidden
+        self._num_classes = num_classes
+        self._batch_size = batch_size
+
+        self._weight_initializer = tf.variance_scaling_initializer()
+        self._bias_initializer = tf.constant_initializer(0.0)
+
+        # Initialize the stuff you need
+        self._inputs = tf.placeholder(tf.float32,
+                                      shape=[self._input_length, self._batch_size, self._input_dim],
+                                      name='inputs')
+
+        self._targets = tf.placeholder(tf.float32,
+                                       shape=[self._batch_size, self._num_classes],
+                                       name='targets')
+
+        self._logits = self.compute_logits()
+        self._loss = self.compute_loss()
+        self._accuracy = self.accuracy()
+
+    def _rnn_step(self, h_prev, x):
+        # Single step through Vanilla RNN cell ...
+        with tf.variable_scope('rnn_step'):
+
+            Whh = tf.get_variable(name='Whh',
+                                  shape=[self._num_hidden, self._num_hidden],
+                                  dtype=tf.float32,
+                                  initializer=self._weight_initializer)
+
+            Whx = tf.get_variable(name='Whx',
+                                  shape=[self._input_dim, self._num_hidden],
+                                  dtype=tf.float32,
+                                  initializer=self._weight_initializer)
+
+            bh = tf.get_variable(name='bh',
+                                 shape=[self._num_hidden],
+                                 dtype=tf.float32,
+                                 initializer=self._bias_initializer)
+
+            mmulx = tf.matmul(x, Whx)
+            mmulh = tf.matmul(h_prev, Whh)
+            preact = mmulx + mmulh + bh
+            h = tf.tanh(preact)
+
+        return h
+
+    def _get_hidden_states(self):
+        with tf.variable_scope('hidden_states'):
+            initial_state = tf.zeros([self._batch_size, self._num_hidden], name='initial_state')
+            states = tf.scan(self._rnn_step, self._inputs,
+                             initializer=initial_state, name='hidden_states')
+        return states
+
+    def compute_logits(self):
+        # Implement the logits for predicting the last digit in the palindrome
+        states = self._get_hidden_states()
+        states = states[-1, :, :]
+
+        with tf.variable_scope("logits"):
+            Woh = tf.get_variable(name='Woh',
+                                  shape=[self._num_hidden, self._num_classes],
+                                  dtype=tf.float32,
+                                  initializer=self._weight_initializer)
+
+            bo = tf.get_variable(name='bo',
+                                 shape=[self._num_classes],
+                                 dtype=tf.float32,
+                                 initializer=self._bias_initializer)
+
+            logits = tf.add(tf.matmul(states, Woh), bo, name="logits")
+        return logits
+
+    def compute_loss(self):
+        # Implement the cross-entropy loss for classification of the last digit
+
+        with tf.variable_scope("loss"):
+            loss = tf.nn.softmax_cross_entropy_with_logits(labels=self._targets,
+                                                           logits=self._logits)
+            loss = tf.reduce_mean(loss, name="softmax")
+            tf.summary.scalar('mean_cross_entropy', loss)
+
+        return loss
+
+    def accuracy(self):
+        # Implement the accuracy of predicting the
+        # last digit over the current batch ...
+        with tf.variable_scope("accuracy"):
+            pred_class = tf.argmax(input=self._logits, axis=1)
+            true_class = tf.argmax(input=self._targets, axis=1)
+            accuracy = tf.reduce_mean(tf.cast(tf.equal(pred_class, true_class), tf.float32))
+            tf.summary.scalar('accuracy', accuracy)
+            tf.summary.histogram('pred_class', pred_class)
+
+        return accuracy
